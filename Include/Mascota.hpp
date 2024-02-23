8f29dc7fe525989b3ec1b5a7a3ceccac1995564e
LeonardoBriseno22310185 <160084527+LeonardoBriseno22310185@users.noreply.github.com>
2024-02-23 02:15:43 +0000

    Version 2

---
+++ Include/Mascota.hpp
@@ -0,0 +1,14 @@
+#pragma once
+class Mascota
+{
+private:
+    int vida;
+public:
+    Mascota() {
+        this->vida=0
+    }
+    ~Mascota() {}
+    void Comer(alimento Alimento){
+        vida += alimento. Extraer alimento
+    }
+};
